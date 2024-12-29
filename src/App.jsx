124d1fb3e80b434c5dbdcea94e8106b96cc71d9a
rajuboppa <rajupatelboppa@gmail.com>
2024-12-29 16:07:53 -0500

    changing component composition to somwhat handle prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import Product from "./components/Product.jsx";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -67,11 +69,21 @@ function App() {
 
   return (
     <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <CartContext.Provider>
+        <Header
+          cart={shoppingCart}
+          onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+        />
+        <Shop>
+          <ul id="products">
+            {DUMMY_PRODUCTS.map((product) => (
+              <li key={product.id}>
+                <Product {...product} onAddToCart={handleAddItemToCart} />
+              </li>
+            ))}
+          </ul>{" "}
+        </Shop>
+      </CartContext.Provider>
     </>
   );
 }
